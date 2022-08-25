29ccbb257ed5e85d9ba766b7c5f4fb7b3bbfae4e
Juan Manuel Zimmermann <juanmanuelzimmermann@gmail.com>
2022-08-25 01:39:32 -0300

    Switch case undefined

--- app.js
+++ app.js
@@ -2,26 +2,23 @@ const archivo = require('./funcionesDeTareas')
 
 let arrayTareas = archivo.leerArchivo();
 
-if (process.argv.length < 3) {
-    console.log('Atención - Tienes que pasar una acción.')
-    console.log('Acciones disponibles: listar')
-} else {
-    let accion = process.argv[2]
+let accion = process.argv[2]
 
-    switch (accion) {
-        case "listar":
-            console.log('Tareas:')
-            for (i = 0; i < arrayTareas.length; i++) {
-                console.log(
-                    (i + 1 + '. ' + arrayTareas[i].titulo + ' - ' + arrayTareas[i].estado)
-                )
-            }
-            //console.log(arrayTareas)
-            break;
-        default:
-            console.log('No entiendo qué quieres hacer.')
-            console.log('Acciones disponibles: listar')
-            break;
-    }
+switch (accion) {
+    case "listar":
+        console.log('Tareas:')
+        for (i = 0; i < arrayTareas.length; i++) {
+            console.log(
+                (i + 1 + '. ' + arrayTareas[i].titulo + ' - ' + arrayTareas[i].estado)
+            )
+        }
+        break;
+    case undefined:
+        console.log('Tienes que poner una accion')
+        console.log('Acciones disponibles: listar')
+        break;
+    default:
+        console.log('No entiendo qué quieres hacer.')
+        console.log('Acciones disponibles: listar')
+        break;
 }
-//console.log(arrayTareas[0]);
