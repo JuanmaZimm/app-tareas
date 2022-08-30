8c8536ad3885649f079df3a65d94672896cb9250
Juan Manuel Zimmermann <juanmanuelzimmermann@gmail.com>
2022-08-29 22:33:47 -0300

    Reset de tareas.json + mensaje

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let accion = process.argv[2]
 
 function Tarea(titulo) {
     this.titulo = titulo;
-    this.estado = "pendiente"
+    this.estado = "Pendiente"
 }
 
 switch (accion) {
@@ -20,6 +20,9 @@ switch (accion) {
         let tituloTarea = process.argv[3]
         let tareaNueva = new Tarea(tituloTarea)
         archivo.guardarTarea(tareaNueva)
+        console.log("Creando:")
+        console.log('Tarea: ' + tareaNueva.titulo)
+        console.log('Estado: ' + tareaNueva.estado)
         break;
     case undefined:
         console.log('Tienes que poner una accion')
